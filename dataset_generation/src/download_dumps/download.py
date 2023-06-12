ba9d06df0e3013305b8bf809d0133870c18a20c4
alessandro <scire@babelscape.com>
2023-06-12 09:06:04 +0200

    wikisource dump date is now configurable.

--- dataset_generation/src/download_dumps/download.py
+++ dataset_generation/src/download_dumps/download.py
@@ -1,15 +1,23 @@
 from typing import List
 from dataset_generation.src.download_dumps.download_gutenberg import download_gutenberg
-from dataset_generation.src.download_dumps.download_wiki import download_wiki, organize_texts
-from dataset_generation.src.utils.constants import WIKI_DUMP_PATH, WIKI_DUMP_DATE, WIKISOURCE_DUMP_PATH, \
-    GUTENBERG_DATASET_PATH
+from dataset_generation.src.download_dumps.download_wiki import (
+    download_wiki,
+    organize_texts,
+)
+from dataset_generation.src.utils.constants import (
+    WIKI_DUMP_PATH,
+    WIKI_DUMP_DATE,
+    WIKISOURCE_DUMP_PATH,
+    GUTENBERG_DATASET_PATH,
+    WIKISOURCE_DUMP_DATE,
+)
 
 
 def download(languages: List[str], debug_title: str = None):
     # downloads and parses wikipedia
     download_wiki("wiki", WIKI_DUMP_PATH, WIKI_DUMP_DATE, languages)
     # downloads and parses wikisource
-    download_wiki("wikisource", WIKISOURCE_DUMP_PATH, WIKI_DUMP_DATE, languages)
+    download_wiki("wikisource", WIKISOURCE_DUMP_PATH, WIKISOURCE_DUMP_DATE, languages)
     # cleans and organizes wikisource book texts
     organize_texts(
         WIKISOURCE_DUMP_PATH, WIKI_DUMP_DATE, languages, debug_title=debug_title
