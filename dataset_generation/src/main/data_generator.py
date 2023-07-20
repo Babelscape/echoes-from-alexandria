88e66614f7095ebd5961477a79dc9be0a14d8aa0
alessandro <scire@babelscape.com>
2023-07-20 11:27:11 +0200

    added train, val, test splits

--- dataset_generation/src/main/data_generator.py
+++ dataset_generation/src/main/data_generator.py
@@ -1,11 +1,13 @@
 import json
 import os.path
+import random
 from copy import deepcopy
 from typing import List, Optional
 from dataset_generation.src.echo_wiki.parser import EchoWikiParser
 from dataset_generation.src.echo_xsum.parser import EchoXSumParser
 from dataset_generation.src.main.clean_books import clean_books
 from dataset_generation.src.main.organize_books import organize_books
+from dataset_generation.src.utils import constants
 
 
 def generate_dataset(
@@ -32,8 +34,54 @@ def generate_dataset(
     clean_books(
         output_path
     )
-    index_output_path = output_path.replace(".jsonl", "_index.jsonl")
     out_dir = '/'.join(output_path.split('/')[:-1])
+    dump_datasets(out_dir, output_path)
+    splits = {'train', 'val', 'test'}
+    for dataset_name in {'echo-wiki', 'echo-xsum'}:
+        with open(os.path.join(out_dir, dataset_name + '.jsonl')) as f:
+            dataset = [json.loads(l.strip()) for l in f.readlines()]
+        shuffle = not all([os.path.exists(os.path.join(constants.BASE_PATH, split + '-' + dataset_name + '-ids.tsv')) for split in splits])
+        if shuffle:
+            random_splits(dataset, dataset_name, out_dir)
+        else:
+            split_as_in_paper(dataset, dataset_name, out_dir, splits)
+
+
+def split_as_in_paper(dataset, dataset_name, out_dir, splits):
+    for split in splits:
+        with open(os.path.join(constants.BASE_PATH, split + '-' + dataset_name + '-ids.tsv')) as f:
+            split_ids = [l.strip() for l in f.readlines()]
+            split_dataset = [s for s in dataset if s['synset'] in split_ids]
+            with open(os.path.join(out_dir, f'{split}_' + dataset_name + '.jsonl'), 'w') as w:
+                w.write('\n'.join([json.dumps(s) for s in split_dataset]))
+
+
+
+
+def random_splits(dataset, dataset_name, out_dir):
+    train_perc, val_perc, test_perc = 0.8, 0.1, 0.1
+    random.shuffle(dataset)
+    train_len = len(dataset) * train_perc
+    val_len = len(dataset) * val_perc
+    train_dataset = dataset[:train_len]
+    val_dataset = dataset[train_len: train_len + val_len]
+    test_dataset = dataset[train_len + val_len:]
+    train_ids = set([l['synset'] for l in train_dataset])
+    val_ids = set([l['synset'] for l in val_dataset])
+    test_ids = set([l['synset'] for l in test_dataset])
+    assert len(train_ids.intersection(val_ids)) == 0
+    assert len(train_ids.intersection(test_ids)) == 0
+    assert len(val_ids.intersection(test_ids)) == 0
+    with open(os.path.join(out_dir, 'train_' + dataset_name + '.jsonl'), 'w') as w:
+        w.write('\n'.join([json.dumps(s) for s in train_dataset]))
+    with open(os.path.join(out_dir, 'validation_' + dataset_name + '.jsonl'), 'w') as w:
+        w.write('\n'.join([json.dumps(s) for s in val_dataset]))
+    with open(os.path.join(out_dir, 'test_' + dataset_name + '.jsonl'), 'w') as w:
+        w.write('\n'.join([json.dumps(s) for s in test_dataset]))
+
+
+def dump_datasets(out_dir, output_path):
+    index_output_path = output_path.replace(".jsonl", "_index.jsonl")
     echo_wiki_path = os.path.join(out_dir, 'echo-wiki.jsonl')
     echo_xsum_path = os.path.join(out_dir, 'echo-xsum.jsonl')
     with open(index_output_path) as f:
